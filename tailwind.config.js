bb1bcf82a0423cda5d66bb43b8853955e31f2aa5
tashenoff <kail.dead.kd@gmail.com>
2022-07-15 21:22:23 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0566AC',
+        darkLight: '#0c162d',
+        primarylight: '#1B90E3',
+        secondary: '#A5D7DA',
+        secondaryLight: '#ef238a',
+        green: '#47E189',
+        greenDark: '#42cf7e',
+        black: '#333',
+        white: '#fff',
+        red: '#e045e6',
+        gray: '#F4F4F4',
+        blueLight: '#c3d9f6'
+      }
+    },
   },
   plugins: [],
 }
